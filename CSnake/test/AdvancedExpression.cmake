810c105a39062a3452407baeced49609a582504e
Karthik Nishanth <nishanthkarthik@live.com>
2020-08-29 13:35:16 +0200

    Add more examples

--- CSnake/test/AdvancedExpression.cmake
+++ CSnake/test/AdvancedExpression.cmake
@@ -3,14 +3,17 @@ message(STATUS "Hello world")
 set(X 2)
 
 py_expr(
-    EXPRESSION
-        "math.sqrt(float(X))"
-    MODULES
-        math
-    INPUTS
-        X
-    OUTPUT
-        Y
+    EXPRESSION  "math.sqrt(float(X))"
+    MODULES     math
+    INPUTS      X
+    OUTPUT      Y
+)
+
+py_expr(
+    EXPRESSION "math.pi"
+    MODULES math
+    OUTPUT PI_VALUE
 )
 
 message(STATUS "sqrt(${X}) = ${Y}")
+message(STATUS "PI = ${PI_VALUE}")
